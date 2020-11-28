6a18485614470de5c70cbb22b69ff59e30d05663
Raimund Canzler <raimund.canzler@t-online.de>
2020-11-28 20:58:01 +0100

    Added types.

--- storyblok-vue.ts
+++ storyblok-vue.ts
@@ -4,33 +4,43 @@ declare global {
   }
 }
 
-var addClass = function(el, className) {
+var addClass = function (el: HTMLElement, className: string) {
   if (el.classList) {
-    el.classList.add(className)
-  } else if (!new RegExp('\\b'+ className+'\\b').test(el.className)) {
-    el.className += ' ' + className
+    el.classList.add(className);
+  } else if (!new RegExp("\\b" + className + "\\b").test(el.className)) {
+    el.className += " " + className;
   }
-}
+};
 
 export default {
   install: (app) => {
-    app.directive('editable', {
-      mounted: function(el, binding) {
-        if (typeof binding.value._editable === 'undefined' || binding.value._editable === null) {
-          return
+    app.directive("editable", {
+      mounted: function (el: HTMLElement, binding: any) {
+        if (
+          typeof binding.value._editable === "undefined" ||
+          binding.value._editable === null
+        ) {
+          return;
         }
 
-        var options = JSON.parse(binding.value._editable.replace('<!--#storyblok#', '').replace('-->', ''))
+        var options = JSON.parse(
+          binding.value._editable
+            .replace("<!--#storyblok#", "")
+            .replace("-->", "")
+        );
 
-        el.setAttribute('data-blok-c', JSON.stringify(options))
-        el.setAttribute('data-blok-uid', options.id + '-' + options.uid)
+        el.setAttribute("data-blok-c", JSON.stringify(options));
+        el.setAttribute("data-blok-uid", options.id + "-" + options.uid);
 
-        addClass(el, 'storyblok__outline')
+        addClass(el, "storyblok__outline");
       }
-    })
+    });
 
-    if (typeof window !== 'undefined' && typeof window.storyblok !== 'undefined') {
-      app.config.globalProperties.$storyblok = window.storyblok
+    if (
+      typeof window !== "undefined" &&
+      typeof window.storyblok !== "undefined"
+    ) {
+      app.config.globalProperties.$storyblok = window.storyblok;
     }
   }
-}
+};
